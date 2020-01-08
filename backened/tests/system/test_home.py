1eda739537c7b7e8ae5f969f7a3e001b82bc93ad
chepkoy <allankiplangat22@outlook.com>
2020-01-08 12:59:07 +0300

    Project Pipeline setup

---
+++ backened/tests/system/test_home.py
@@ -0,0 +1,17 @@
+from base_test import BaseTest
+import unittest
+import json
+
+
+class TestHome(BaseTest):
+    def test_home(self):
+        with self.app() as c:
+            resp = c.get('/')
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(json.loads(resp.get_data()),
+                             {'message': 'Hello, World'})
+
+
+if __name__ == "__main__":
+    unittest.main()
